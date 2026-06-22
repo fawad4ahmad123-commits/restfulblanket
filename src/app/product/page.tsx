bd8f97f2887be5c1cc9f093a52ba985b86f41def
Fawad Ahmad <fawad4ahmad123@gmail.com>
2026-06-22 06:59:09 -0700

    fix build issuse

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,18 +1,10 @@
 import ProductContent from "@/src/components/products/product-main";
 import { Suspense } from "react";
 
-const ProductPage = () => {
+export default function ProductPage() {
   return (
-    <Suspense
-      fallback={
-        <main className="min-h-screen bg-[#fdf9f6] px-4 py-8 md:px-8 lg:px-20">
-          <div className="mx-auto max-w-7xl">Loading product…</div>
-        </main>
-      }
-    >
+    <Suspense fallback={<div>Loading...</div>}>
       <ProductContent />
     </Suspense>
   );
-};
-
-export default ProductPage;
+}
