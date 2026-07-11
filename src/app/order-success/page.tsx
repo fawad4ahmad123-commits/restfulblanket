73ac7d9379182898e8229803aefbb1beace9549d
Fawad Ahmad <fawad4ahmad123@gmail.com>
2026-07-11 04:04:47 -0700

    add protected route

--- src/app/order-success/page.tsx
+++ src/app/order-success/page.tsx
@@ -1,17 +1,20 @@
 import OrderConfirmClient from '@/src/components/checkout/order-confirm';
 import { Loader } from '@/src/components/loader';
+import { ProtectedRoute } from '@/src/core/context/auth-context';
 import { Suspense } from 'react';
 
 export default function Page() {
   return (
-    <Suspense
-      fallback={
-        <div className="p-10 text-center">
-          <Loader />
-        </div>
-      }
-    >
-      <OrderConfirmClient />
-    </Suspense>
+    <ProtectedRoute>
+      <Suspense
+        fallback={
+          <div className="p-10 text-center">
+            <Loader />
+          </div>
+        }
+      >
+        <OrderConfirmClient />
+      </Suspense>
+    </ProtectedRoute>
   );
 }
